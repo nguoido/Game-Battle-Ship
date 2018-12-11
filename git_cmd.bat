123f0699df6caabb4059bc238c7634de30e948de
dovankhai <nguoido97@gmail.com>
2018-12-11 23:52:03 +0700

    o

--- git_cmd.bat
+++ git_cmd.bat
@@ -1,3 +1,27 @@
+@echo off
+REM XET KHUNG HINH
+mode con:cols=60 lines=23
+title %~nx0
+echo ------------------------------------------------------------
+echo ---------------------     GitHup     ---------------------
+echo ------------------------------------------------------------
+
+echo 1. pull
+echo 2. push
+
+echo ------------------------------------------------------------
+set p= 
+set /p p= OPTION:
+
+if %p% == 1 goto option1 
+if %p% == 2 goto option2
+
+:option1
+start git pull
+pause
+
+:option2
 git add *
 git commit -m "o"
 git push
+exit

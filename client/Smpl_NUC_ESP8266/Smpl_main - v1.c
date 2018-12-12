1fd804dbe28b35f08988dee71a78a5a8431b470a
dovankhai <nguoido97@gmail.com>
2018-12-12 22:32:26 +0700

    update

--- client/Smpl_NUC_ESP8266/Smpl_main - v1.c
+++ client/Smpl_NUC_ESP8266/Smpl_main - v1.c
@@ -21,10 +21,9 @@
 #define AT_CIPAPMAC "AT+CIPAPMAC?\r\n"
 #define AT_CWMODE "AT+CWMODE=1\r\n"
 #define AT_CIPMUX "AT+CIPMUX=0\r\n"
-#define AT_CWJAP "AT+CWJAP=\"MYH 1\",\"12022017\"\r\n"
-//#define AT_CWJAP "AT+CWJAP=\"thuy\",\"hhhhhhhh\"\r\n"
-#define AT_CIPSTART "AT+CIPSTART=\"TCP\",\"13.250.115.86\",3333\r\n"
-#define AT_CIPSEND_1 "AT+CIPSEND=1\r\n"
+//#define AT_CWJAP "AT+CWJAP=\"MYH 1\",\"12022017\"\r\n"
+#define AT_CWJAP "AT+CWJAP=\"thuy\",\"hhhhhhhh\"\r\n"
+#define AT_CIPSTART "AT+CIPSTART=\"TCP\",\"34.205.32.160\",3333\r\n"
 
 volatile uint8_t buf_Mac[17];
 volatile uint16_t i = 0;
@@ -48,9 +47,9 @@ void UART_INT_HANDLE(void)
 		DrvUART_Read(UART_PORT0,bInChar,1);	
 		if(bInChar[0] == '^')
 		{
-			DrvGPIO_ClrBit(E_GPC,0); 		// GPB11 = 0 to turn on Buzzer
-			delay_time(1);   				 		// Delay 
-			DrvGPIO_SetBit(E_GPC,0); 		// GPB11 = 1 to turn off Buzzer	
+			DrvGPIO_ClrBit(E_GPC,0); // GPB11 = 0 to turn on Buzzer
+			delay_time(1);    // Delay 
+			DrvGPIO_SetBit(E_GPC,0); // GPB11 = 1 to turn off Buzzer	
 		}
 		else if(bInChar[0] == '"')
 		{
@@ -58,12 +57,14 @@ void UART_INT_HANDLE(void)
 		}
 		else if(flag == 1)
 		{
-			buf_Mac[i] = bInChar[0];
-			i++;
+			 buf_Mac[i] = bInChar[0];
+			 i++;
 		}
-		
+
 	}
 }
+
+
 
 int main()
 {
@@ -71,21 +72,28 @@ int main()
 	interruptConfig();
 	uartConfig();
 	
-	DrvGPIO_Open(E_GPC, 0, E_IO_OUTPUT); 
+	DrvGPIO_Open(E_GPC, 0, E_IO_OUTPUT); // initial controlling 
 	
 	DrvUART_Write(UART_PORT0, (unsigned char*)AT_CWMODE, strlen(AT_CWMODE));
-	DrvSYS_Delay(1000);
+	delay_time(5);
 	DrvUART_Write(UART_PORT0, (unsigned char*)AT_CIPMUX, strlen(AT_CIPMUX));
-	DrvSYS_Delay(10000);
+	delay_time(5);
 	DrvUART_Write(UART_PORT0, (unsigned char*)AT_CWJAP, strlen(AT_CWJAP));
 	delay_time(30);
+
+	
 	DrvUART_EnableInt(UART_PORT0, DRVUART_RDAINT, (PFN_DRVUART_CALLBACK *)UART_INT_HANDLE); 
+
 	DrvUART_Write(UART_PORT0, (unsigned char*)AT_CIPAPMAC, strlen(AT_CIPAPMAC));
 	delay_time(5);
+	
 	DrvUART_Write(UART_PORT0, (unsigned char*)AT_CIPSTART, strlen(AT_CIPSTART));
 	delay_time(30);
+	while(1)
+	{	
+		
+	}
 	
-	while(1){}
 }
 
 void delay_time(int time)		     
@@ -93,7 +101,7 @@ void delay_time(int time)
 	int i=0;
 	for(i=0;i<time*3;i++)
 	{
-		DrvSYS_Delay(300000);
+		DrvSYS_Delay(330000);
 	}
 }
 
@@ -103,22 +111,24 @@ void initLed(void){
 	DrvGPIO_ClrBit(E_GPA, 15);            // Goutput Hi to turn off LED
 	DrvGPIO_Open(E_GPA, 14, E_IO_OUTPUT); // GPC12 pin set to output mode
 	DrvGPIO_ClrBit(E_GPA, 14);            // Goutput Hi to turn off LED
+	
 }
 
-/*
-	void uart_sendStr(uint8_t *str)
-	{
+
+void uart_sendStr(uint8_t *str)
+{
 	while(*str)
 	{
-	DrvUART_Write(UART_PORT0,str,1);
-	DrvSYS_Delay(20000);
-	str++;
+			DrvUART_Write(UART_PORT0,str,1);
+			DrvSYS_Delay(20000);
+			str++;
 	}
-	}
-*/
+}
+
 
 void uartConfig(void){
 	STR_UART_T myuart;
+	
 	DrvGPIO_InitFunction(E_FUNC_UART0);   
 	
 	/* UART Setting */
@@ -133,83 +143,69 @@ void uartConfig(void){
 	DrvGPIO_SetBit(E_GPC,14);
 }
 
+
+
+
 void interruptConfig(){	
 	DrvGPIO_Open(E_GPA,12,E_IO_INPUT);
 	DrvGPIO_Open(E_GPA,13,E_IO_INPUT);
-	DrvGPIO_EnableInt(E_GPA, 12, E_IO_RISING, E_MODE_EDGE);		//BTN OK
-	DrvGPIO_EnableInt(E_GPA, 13, E_IO_RISING, E_MODE_EDGE);		//BTN CANCLE
+  DrvGPIO_EnableInt(E_GPA, 12, E_IO_RISING, E_MODE_EDGE);		//BTN OK
+  DrvGPIO_EnableInt(E_GPA, 13, E_IO_RISING, E_MODE_EDGE);		//BTN CANCLE
 	DrvGPIO_EnableInt(E_GPC, 1, E_IO_RISING, E_MODE_EDGE);		//BTN DOWN
-	DrvGPIO_EnableInt(E_GPC, 2, E_IO_RISING, E_MODE_EDGE);		//BTN RIGHT
+  DrvGPIO_EnableInt(E_GPC, 2, E_IO_RISING, E_MODE_EDGE);		//BTN RIGHT
 	DrvGPIO_EnableInt(E_GPC, 3, E_IO_RISING, E_MODE_EDGE);		//BTN LEFT
-	DrvGPIO_EnableInt(E_GPD, 7, E_IO_RISING, E_MODE_EDGE);		//BTN UP
-	DrvGPIO_SetDebounceTime(5, E_DBCLKSRC_10K);
+  DrvGPIO_EnableInt(E_GPD, 7, E_IO_RISING, E_MODE_EDGE);		//BTN UP
+  DrvGPIO_SetDebounceTime(5, E_DBCLKSRC_10K);
 	DrvGPIO_EnableDebounce(E_GPA, 12);
 	DrvGPIO_EnableDebounce(E_GPA, 13);
 	DrvGPIO_EnableDebounce(E_GPC, 1);
 	DrvGPIO_EnableDebounce(E_GPC, 2);
 	DrvGPIO_EnableDebounce(E_GPC, 3);
 	DrvGPIO_EnableDebounce(E_GPD, 7);
-	DrvGPIO_SetIntCallback(GPIOAB_INT_CallBack, GPIOCDE_INT_CallBack);
+  DrvGPIO_SetIntCallback(GPIOAB_INT_CallBack, GPIOCDE_INT_CallBack);
 }
 
 void GPIOCDE_INT_CallBack(uint32_t GPC_IntStatus, uint32_t GPD_IntStatus, uint32_t GPE_IntStatus)
 {  
 	if ((GPC_IntStatus>>1) & 0x01){
-		DrvUART_Write(UART_PORT0,(unsigned char*)AT_CIPSEND_1, 16);
-		DrvSYS_Delay(1000);
-		DrvUART_Write(UART_PORT0,(unsigned char*)"d", 1);
-		DrvSYS_Delay(20);
+		uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
+		DrvSYS_Delay(2);
+		uart_sendStr((unsigned char*)"d");
 	}
-	else if ((GPC_IntStatus>>2) & 0x01){
-		// Cach 1
-		/*
-			uart_sendStr((unsigned char*)"AT+CIPSEND\r\n");
-			DrvSYS_Delay(20000);
-			uart_sendStr((unsigned char*)"r");
-			DrvSYS_Delay(20000);
-			uart_sendStr((unsigned char*)"+++");
-			DrvSYS_Delay(20000);
-		*/
-		
-		// Cach 2
-		/*
-			//uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
-			//DrvSYS_Delay(2);
-			//uart_sendStr((unsigned char*)"r");
-		*/
-		
-		DrvUART_Write(UART_PORT0,(unsigned char*)AT_CIPSEND_1, 16);
-		DrvSYS_Delay(1000);
-		DrvUART_Write(UART_PORT0,(unsigned char*)"r", 1);
-		DrvSYS_Delay(20);
+	if ((GPC_IntStatus>>2) & 0x01){
+		//uart_sendStr((unsigned char*)"AT+CIPSEND\r\n");
+		//DrvSYS_Delay(20000);
+		//uart_sendStr((unsigned char*)"r");
+		//DrvSYS_Delay(20000);
+		//uart_sendStr((unsigned char*)"+++");
+		//DrvSYS_Delay(20000);
+		uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
+		DrvSYS_Delay(2);
+		uart_sendStr((unsigned char*)"r");
 	}
-	else if ((GPC_IntStatus>>3) & 0x01){
-		DrvUART_Write(UART_PORT0,(unsigned char*)AT_CIPSEND_1, 16);
-		DrvSYS_Delay(1000);
-		DrvUART_Write(UART_PORT0,(unsigned char*)"l", 1);
-		DrvSYS_Delay(20);
-		
+	if ((GPC_IntStatus>>3) & 0x01){
+		uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
+		DrvSYS_Delay(2);
+		uart_sendStr((unsigned char*)"l");
+	
 	}
-	else if ((GPD_IntStatus>>7) & 0x01){
-		DrvUART_Write(UART_PORT0,(unsigned char*)AT_CIPSEND_1, 16);
-		DrvSYS_Delay(1000);
-		DrvUART_Write(UART_PORT0,(unsigned char*)"u", 1);
-		DrvSYS_Delay(20);
-	}
+	if ((GPD_IntStatus>>7) & 0x01){
+		uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
+		DrvSYS_Delay(2);
+		uart_sendStr((unsigned char*)"u");
+		}
 }
 
 void GPIOAB_INT_CallBack(uint32_t GPA_IntStatus, uint32_t GPB_IntStatus)
 {
 	if ((GPA_IntStatus>>12) & 0x01){
-		DrvUART_Write(UART_PORT0,(unsigned char*)AT_CIPSEND_1, 16);
-		DrvSYS_Delay(1000);
-		DrvUART_Write(UART_PORT0,(unsigned char*)"s", 1);
-		DrvSYS_Delay(20);
+		uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
+		DrvSYS_Delay(2);
+		uart_sendStr((unsigned char*)"s");
 	}
-	else if ((GPA_IntStatus>>13) & 0x01){
-		DrvUART_Write(UART_PORT0,(unsigned char*)AT_CIPSEND_1, 16);
-		DrvSYS_Delay(1000);
-		DrvUART_Write(UART_PORT0,(unsigned char*)"c", 1);
-		DrvSYS_Delay(20);
+	if ((GPA_IntStatus>>13) & 0x01){
+		uart_sendStr((unsigned char*)"AT+CIPSEND=1\r\n");
+		DrvSYS_Delay(2);
+		uart_sendStr((unsigned char*)"c");
 	}
 }
